16083bd750d6203e431992032d484767a49cf16a
Daniel Cirne Vilas-Boas <dcirne@motorola.com>
2019-06-02 18:02:44 -0300

    implementando o teste nao parametrico de wilcoxon

--- src/Test.py
+++ src/Test.py
@@ -12,12 +12,15 @@ def main():
     # random_data.append(bkc.return_view_sample(1, random_index))
     # random_data.append(bkc.return_view_sample(2, random_index))
     # bkc.check_max_probability(random_data)
-    ExperimentUtil.calculate_error_margin(list(), 0.95)
-    bkc = BayesianKneighborClassifier('../data_bases/daniel/crisp-partition.txt')
-    gbc = GaussianBayesClassifier('../data_bases/daniel/crisp-partition.txt')
-    random_index = randint(0, 2000)
+    list_1 = [30, 19, 19, 23, 29, 178, 42 , 20, 12, 39, 14, 81, 17, 31, 52]
+    list_2 = [30, 6, 14, 8, 14, 52, 14, 22, 17, 8, 11, 30, 14, 17, 15]
+    ExperimentUtil.perform_wilcoxon_validation(list_1, list_2)
+    # ExperimentUtil.calculate_error_margin(list(), 0.95)
+    # bkc = BayesianKneighborClassifier('../data_bases/daniel/crisp-partition.txt')
+    # gbc = GaussianBayesClassifier('../data_bases/daniel/crisp-partition.txt')
+    # random_index = randint(0, 2000)
 
-    bkc.check_max_probability(random_index)
+    #bkc.check_max_probability(random_index)
 
 
 if __name__ == '__main__':
--- src/util/ExperimentUtil.py
+++ src/util/ExperimentUtil.py
@@ -5,8 +5,14 @@ import scipy.stats as st
 
 class ExperimentUtil:
 
+    def __init__(self):
+        pass
+
+    # extracted from: http://www2.fm.usp.br/dim/info/wilcoxon/index.php
+    wilcox_table = [0, 0, 0, 0, 0, 0, 0, 2, 3, 5, 8, 10, 13, 17, 21, 25]
+
     @staticmethod
-    def calculate_error_margin(input_data, confidence_coefficient):
+    def _calculate_error_margin(input_data, confidence_coefficient):
         critical_value = st.norm.ppf(1 - (1 - confidence_coefficient) / 2)
         std_error = statistics.stdev(input_data) / math.sqrt(len(input_data))
         return critical_value * std_error
@@ -17,8 +23,73 @@ class ExperimentUtil:
         """"The method calculates de confidence interval superior and inferior limits
         The code implementation was based on this link: https://pt.wikihow.com/Calcular-o-Intervalo-de-Confian%C3%A7a
         """
-        error_margin = ExperimentUtil.calculate_error_margin(input_data, confidence_coeficient)
+        error_margin = ExperimentUtil._calculate_error_margin(input_data, confidence_coeficient)
         superior_limit = statistics.mean(input_data) + error_margin
         inferior_limit = statistics.mean(input_data) - error_margin
         return superior_limit, inferior_limit
+
+    @staticmethod
+    def _calculate_differences(classifier1, classifier2):
+        differences = []
+        sorted_diffs = []
+        for index, score in enumerate(classifier1):
+            difference = score - classifier2[index]
+            if difference:
+                differences.append(score - classifier2[index])
+                sorted_diffs.append(abs(score - classifier2[index]))
+        return differences, sorted_diffs
+
+    @staticmethod
+    def _calculate_position_differences(differences, sorted_diffs):
+        position_diffs = []
+        for index, diff in enumerate(differences):
+            count = sorted_diffs.count(abs(diff))
+            if count > 1:
+                median = sorted_diffs.index(abs(diff)) + 1
+                for i in range(1, count):
+                    median += sorted_diffs.index(abs(diff)) + i + 1
+                    position_diffs.append(float(median)/count)
+            else:
+                position_diffs.append(sorted_diffs.index(abs(diff)) + 1)
+        return position_diffs
+
+    @staticmethod
+    def _calculate_positive_negative_sum(position_diffs):
+        sum_positive = 0
+        sum_negative = 0
+        for score in position_diffs:
+            if score < 0:
+                sum_negative += abs(score)
+            else:
+                sum_positive += score
+        return sum_positive, sum_negative
+
+    @staticmethod
+    def perform_wilcoxon_validation(series1, series2):
+        """This method performs Wilcoxon validation. The boolean output means that the null hypothesis
+        was rejected or not. Implemented following the steps on http://www.leg.ufpr.br/lib/exe/fetch.php/disciplinas:ce001:wilcoxon_1_.pdf"""
+        differences, sorted_diffs = ExperimentUtil._calculate_differences(series1, series2)
+        sorted_diffs.sort()
+        position_diffs = ExperimentUtil._calculate_position_differences(differences, sorted_diffs)
+
+        for index, score in enumerate(differences):
+            if score < 0:
+                position_diffs[index] = position_diffs[index] * -1
+
+        sum_positive, sum_negative = ExperimentUtil._calculate_positive_negative_sum(position_diffs)
+        T = min(sum_positive, sum_negative)
+        # TODO: Se o tamanho de n for maior que 30, é preciso usar a tabela T-Student
+        if len(position_diffs) <= 30:
+            # TODO: Com o valor de T, precisamos ver qual o valor critico e elaborar melhor a resposta no relatório
+            return T < ExperimentUtil.wilcox_table[len(position_diffs)]
+
+
+
+
+
+
+
+
+
+
 
