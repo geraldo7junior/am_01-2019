78786e50f452c18abf2326d083781d5bc29a4ede
Saulo Silva <saulo.silva@pe.senai.br>
2019-06-12 20:05:11 -0300

    Definição do melhor J/melhor partição crisp

--- src/Test.py
+++ src/Test.py
@@ -3,7 +3,7 @@ import pandas as pd
 from classifiers.BayesianKNNClassifier import BayesianKNNClassifier
 from classifiers.GaussianBayesClassifier import GaussianBayesClassifier
 from sklearn import preprocessing
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, StratifiedShuffleSplit
 from sklearn import metrics
 import statistics
 
@@ -25,15 +25,16 @@ def main():
     data_mfeat_fac = normalize_data(pd.read_csv('../data_bases/mfeat_fac.csv', header=None))
     data_mfeat_fou = normalize_data(pd.read_csv('../data_bases/mfeat_fou.csv', header=None))
     data_mfeat_kar = normalize_data(pd.read_csv('../data_bases/mfeat_kar.csv', header=None))
-    data_class = np.loadtxt('../data_bases/daniel/crisp-partition.txt', dtype=int)
+    data_class = np.loadtxt('../saidas/Execução-29-crisp-partition.txt', dtype=int)
 
     kf = KFold(n_splits=10, shuffle=True)
+    sss = StratifiedShuffleSplit(n_splits=10)
     medias_bkc = []
     medias_gbc = []
     for x in range(30):
         acc_bkc = []
         acc_gbc = []
-        for train_index, test_index in kf.split(data_mfeat_fac):
+        for train_index, test_index in sss.split(data_mfeat_fac, data_class):
             X1_train, X1_test = data_mfeat_fac[train_index], data_mfeat_fac[test_index]
             X2_train, X2_test = data_mfeat_fou[train_index], data_mfeat_fou[test_index]
             X3_train, X3_test = data_mfeat_kar[train_index], data_mfeat_kar[test_index]
@@ -53,6 +54,25 @@ def main():
         medias_gbc.append(statistics.mean(acc_gbc))
     print("BayesianKNNClassifier: ", medias_bkc)
     print("GaussianBayesClassifier: ", medias_gbc)
+
+
+def best_J():
+    count = 0
+    indices = []
+    for i in range(100):
+        teste = np.loadtxt('../saidas/Execução-{}-crisp-partition.txt'.format(i+1), dtype=int)
+        if len(set(teste)) == 10:
+            count += 1
+            indices.append(i)
+    print(count)
+    print(indices)
+    jotas = []
+    for i in indices:
+        file = open('../saidas/Execução-{}-output.txt'.format(i+1), "r")
+        jotas.append(float(file.readline()[4:-1]))
+        file.close()
+    print(min(jotas))
+    # best J = 1240.8175952498866
 
 
 if __name__ == '__main__':
